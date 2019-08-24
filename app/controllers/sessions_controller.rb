c3bf1d94454061d2e543fb58c0aa526409729628
Alan Krajina <alankrajina88@gmail.com>
2019-08-24 09:29:50 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    session[:name] = params[:name]
+    if session[:name] != nil && session[:name] != ""
+    #  log_in user
+      redirect_to '/'
     else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      redirect_to '/login'
     end
   end
 
+
   def destroy
+    session.destroy
   end
 end
