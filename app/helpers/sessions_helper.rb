6d0b22a36b3dba994e9fe2bebb059987120d57dc
Alan Krajina <alankrajina88@gmail.com>
2019-08-24 10:00:39 +0000

    Automatically backed up by Learn

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,7 @@ module SessionsHelper
   end
 
   # Returns true if the user is logged in, false otherwise.
-#  def logged_in?
-#    !current_user.nil?
-#  end
+  def logged_in?
+    !current_user.nil?
+  end
 end
