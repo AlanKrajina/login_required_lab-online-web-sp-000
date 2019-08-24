c668499d90f11920e67cc6b3b1707b345d0bfa6f
Alan Krajina <alankrajina88@gmail.com>
2019-08-24 09:36:06 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  post   '/logout'   to: 'sessions#destroy'
+  post '/logout' => 'sessions#destroy'
 end
