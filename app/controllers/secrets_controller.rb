90d38ebff3537bdf2a69749c06d864d5fdc38262
Alan Krajina <alankrajina88@gmail.com>
2019-08-24 09:37:14 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,20 +1,7 @@
 class SecretsController < ApplicationController
 
-  def new
-  end
-
-  def create
-    session[:name] = params[:name]
-    if session[:name] != nil && session[:name] != ""
-    #  log_in user
-      redirect_to '/'
-    else
-      redirect_to '/login'
-    end
+  def show
   end
 
 
-  def destroy
-    session.destroy
-  end
 end
