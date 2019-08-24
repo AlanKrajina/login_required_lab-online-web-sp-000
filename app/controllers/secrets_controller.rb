760bbd36afece174ab09d814cd5e889aba51bc37
Alan Krajina <alankrajina88@gmail.com>
2019-08-24 10:00:16 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,15 +1,7 @@
 class SecretsController < ApplicationController
   before_action :require_logged_in
+  
   def show
-    if current_user
-      redirect_to 'sessions/new'
-    end
   end
-
-  private
-
-def require_login
-  return head(:forbidden) unless session.include? session[:name]
-end
 
 end
